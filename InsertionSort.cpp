d867915dca4fc5cd0567a97cf3aae18a8f8d0a66
benjaminraiford <benjaminhraiford@gmail.com>
2019-10-31 01:41:24 -0400

    clean code: only LinkedList InsertionSort included in main

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,34 +1,25 @@
 #include <cassert>
 #include <iostream>
-#include <vector>
-#include <ctime>
-#include "BinaryInsertionSort.h"
-using namespace std;
+#include "LinkedList.h"
 
 int main() {
-
     int seed, length;
-    cin >> seed;
+    cin >> seed >> length;
     srand(seed);
 
+    LinkedList Ben;
+    int randomValue;
 
-    for(int i = 0; i <= 5001; i += 100){
-        length = i;
-        vector<int> v(length);
-
-        // generate vector of random integers
-        for (int & i : v) {
-            i = rand() % 100;
-        }
-
-         // vector — binary insertion sort and clock
-        clock_t start_BinaryInsertionSort = clock();
-        insertionSort(v, v.size());
-        clock_t end_BinaryInsertionSort = clock();
-        double elapsed_BinaryInsertionSort = double(end_BinaryInsertionSort - start_BinaryInsertionSort) / CLOCKS_PER_SEC;
-
-        //gives vector size and time for execution
-        cout << i << "\t" << elapsed_BinaryInsertionSort << endl;
+    //generate vector and linked list of unsorted values
+    for (int i = 0; i < length; i++) {
+        randomValue = rand() % 100;
+        Ben.append(randomValue);
     }
-}
+    std::cout << "Unsorted list:" << std::endl;
+    Ben.printList();
 
+    // linked list — InsertionSort
+    std::cout << "\nSorted list:" << std::endl;
+    Ben.InsertionSort();
+    Ben.printList();
+}
