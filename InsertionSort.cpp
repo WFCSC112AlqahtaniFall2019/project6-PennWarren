98eb9f12043889880358dcd11e831f254879e80b
benjaminraiford <benjaminhraiford@gmail.com>
2019-10-31 00:46:49 -0400

    time trial for LinkedList InsertionSort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,33 +1,33 @@
 #include <cassert>
 #include <iostream>
-#include <vector>
-#include "BinaryInsertionSort.h"
-#include "Node.h"
+#include <ctime>
 #include "LinkedList.h"
 using namespace std;
 
 int main() {
 
     int seed, length;
-    cin >> seed >> length;
+    cin >> seed;
     srand(seed);
-
-    vector<int> v(length);
     LinkedList Ben;
-    int randomValue;
 
-    //generate vector and linked list of unsorted values
-    for (int & i : v) {
-        randomValue = rand() % 100;
-        i = randomValue;
-        Ben.append(randomValue);
+    for(int i = 2; i <= 10002; i = i+100){
+        int randomValue;
+        length = i;
+
+        //generate linked list of unsorted values
+        for (int j = 0; j < length-1; j++) {
+            randomValue = rand() % 100;
+            Ben.append(randomValue);
+        }
+
+        //linked list — InsertionSort and clock
+        clock_t start_InsertionSort = clock();
+        Ben.InsertionSort();
+        clock_t end_InsertionSort = clock();
+        double elapsed_InsertionSort = double(end_InsertionSort - start_InsertionSort) / CLOCKS_PER_SEC;
+
+        //gives linked list size and time for execution
+        cout << i-2 << "\t" << elapsed_InsertionSort << endl;
     }
-
-    // vector — binary insertion sort
-    insertionSort(v, v.size());
-
-    // linked list — InsertionSort
-    Ben.InsertionSort();
-    Ben.printList();
-
 }
